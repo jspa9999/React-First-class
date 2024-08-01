86aa771a87d88ebc6836f2f31c8f32357fd951a7
Jayasree <jspa9999@gmail.com>
2024-08-01 20:02:03 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="br"> Basics of React Js has been started </h1>
+      <h2 className="bs"> Jsxml </h2>
+    </div>
+  );
+}
+
+export default App;
